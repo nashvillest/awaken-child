a523acf5d9882988d25b5a6505c20f8bffe686b3
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-05-24 19:32:43 -0500

    Fix init of hide hook

--- functions.php
+++ functions.php
@@ -2,11 +2,15 @@
 
 if (is_child_theme()) {
     // Hide old archive months (2008-2016)
-    wp_enqueue_script(
-        'hide-old-archive-months',
-        get_stylesheet_directory_uri() . '/js/hide-old-archive-months.js',
-        ['jquery'],
-        false,
-        true
-    );
+    function nashvillest_hide_old_archive_months()
+    {
+        wp_enqueue_script(
+            'hide-old-archive-months',
+            get_stylesheet_directory_uri() . '/js/hide-old-archive-months.js',
+            ['jquery'],
+            false,
+            true
+        );
+    }
+    add_action('admin_enqueue_scripts', 'nashvillest_hide_old_archive_months');
 }
